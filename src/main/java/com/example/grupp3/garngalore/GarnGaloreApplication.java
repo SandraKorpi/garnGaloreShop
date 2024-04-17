9bb57915514fbe88c13e906136823c3c1e5221bc
92sankor <92sankor@gafe.molndal.se>
2024-04-18 00:22:36 +0200

    mappar och klasser

---
+++ src/main/java/com/example/grupp3/garngalore/GarnGaloreApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupp3.garngalore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarnGaloreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarnGaloreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grupp3/garngalore/GarnGaloreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupp3.garngalore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarnGaloreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
