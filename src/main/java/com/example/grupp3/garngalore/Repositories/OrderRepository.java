0b16ad87e9e3fc597c3341a49c3887bf58a48a93
Mathias <143452921+Marwin436@users.noreply.github.com>
2024-04-20 00:03:47 +0200

    Skrivit klart serviceklasserna samt lagt till anslutningsinformation till databasen i application.properties. Även lagt till en OrderService & OrderRepository

--- src/main/java/com/example/grupp3/garngalore/Repositories/OrderRepository.java
+++ src/main/java/com/example/grupp3/garngalore/Repositories/OrderRepository.java
@@ -1,9 +1,9 @@
 package com.example.grupp3.garngalore.Repositories;
 
-import com.example.grupp3.garngalore.Models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import com.example.grupp3.garngalore.Models.Order;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String>{
+public interface OrderRepository extends MongoRepository<Order, String> {
 }
---
+++ src/main/java/com/example/grupp3/garngalore/Services/OrderService.java
@@ -0,0 +1,50 @@
+package com.example.grupp3.garngalore.Services;
+
+import com.example.grupp3.garngalore.Models.Order;
+import com.example.grupp3.garngalore.Repositories.OrderRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public void createOrder(Order order) {
+        orderRepository.save(order);
+    }
+
+    public Order getOrderById(String id) {
+        Optional<Order> optionalProduct = orderRepository.findById(id);
+
+        if (optionalOrder.isPresent()) {
+            return optionalOrder.get();
+        } else {
+            throw new EntityNotFoundException("Order med ID " + id + " hittades inte.");
+        }
+    }
+
+    public void deleteOrder(String id) {
+        if (orderRepository.existsById(id)) {
+            orderRepository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Order med ID " + id + " finns inte i databasen.");
+        }
+    }
+
+    public void updateOrder(String id, Order order) {
+        if (orderRepository.existsById(id)) {
+            order.setId(id);
+            orderRepository.save(order);
+        } else {
+            throw new EntityNotFoundException("Order med ID " + id + " finns inte i databasen.");
+        }
+    }
+
+    public Iterable<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+}
